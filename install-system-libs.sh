9553179f6d395714aae8a02751778064c9d4ba02
Sourabh Desai <sourabhdesai@gmail.com>
2023-03-19 22:22:50 +0000

    update install script + fix syntax error in thirdparty.cmake + add .so files and move them to appropriate directories (https://github.com/MPI-IS/mesh/issues/51#issuecomment-733690943)

--- install-system-libs.sh
+++ install-system-libs.sh
@@ -1,1 +1,4 @@
 sudo apt-get install libboost-dev
+sudo apt-get install build-essential libssl-dev
+sudo apt update && sudo apt upgrade
+sudo apt install cmake
---
+++ mesh/cmake/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/FaceMeshPredictor/voca/mesh/mesh
+# Build directory: /workspaces/FaceMeshPredictor/voca/mesh/mesh/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
